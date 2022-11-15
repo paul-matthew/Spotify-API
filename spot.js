009ae1197d180602bd68c6b88260d812031194cf
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2022-11-14 23:50:57 -0500

    Initial commit

---
+++ spot.js
@@ -0,0 +1,14 @@
+//MENU BUTTON FUNCTION
+function dropdown(){
+    document.getElementById("mydropdown").classList.toggle("showme");
+  
+  }
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.menubtn') && !e.target.matches('.menubar')) {
+    var myDropdownop = document.getElementById("mydropdown");
+      if (myDropdownop.classList.contains('showme')) {
+        myDropdownop.classList.remove('showme');
+      }
+    }
+  }
