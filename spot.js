9dc169b1bb25bf77c4b2ec58bceb8802cd4a0bdc
paul-matthew <116613750+paul-matthew@users.noreply.github.com>
2022-11-20 21:45:16 -0500

    updates

--- spot.js
+++ spot.js
@@ -1,13 +1,11 @@
 //MENU BUTTON FUNCTION
 function dropdown(){
     document.getElementById("mydropdown").classList.toggle("showme");
-  
   }
+
   function myFunction(x) {
     x.classList.toggle("change");
   }
-
-
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(e) {
     if (!e.target.matches('.menubtn') && !e.target.matches('.menubar')) {
